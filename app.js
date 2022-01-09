42a41d4464ccc7636c6f0c13c53fd000f9124b7d
Heesung Park <sdh210408@sdh.hs.kr>
2022-01-09 10:18:42 +0900

    watch me

---
+++ app.js
@@ -0,0 +1,20 @@
+setInterval(() => {
+    const watch = document.getElementById('watch');
+    const format = t => {
+        if (t < 10) t = '0' + t;
+        return t;
+    }
+    let now = new Date();
+    let hour = now.getHours();
+    let min = format(now.getMinutes());
+    let sec = format(now.getSeconds());
+    let msc = parseInt(now.getMilliseconds() / 100);
+
+    let ap = '';
+    
+    if (hour < 12) ap = 'AM'; else ap = 'PM';
+    hour = format(hour % 12);
+
+    watch.innerHTML = `${ap} ${hour}:${min}:${sec}.${msc}`;
+}, 100);
+
